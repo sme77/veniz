0909e150b8f905c2c095191fe1554da6525ac691
mohammad esmaiel emadi <emadi68moh@gmail.com>
2021-10-29 21:04:10 +0330

    add direction and dark light theme

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
-	import 'virtual:windi.css';
+	import { onMount } from 'svelte';
 
 	import { Button } from '../lib';
+
+	let dir: 'rtl' | 'ltr' = 'ltr';
+	let theme = '';
+
+	const handleDirection = () => {
+		const currentDir = document.querySelector('BODY').getAttribute('dir');
+		dir = currentDir === 'ltr' ? 'rtl' : 'ltr';
+		document.querySelector('BODY').setAttribute('dir', dir);
+	};
+
+	const handleTheme = () => {
+		const currentTheme = document.querySelector('HTML').classList[0];
+		theme = currentTheme === 'light' ? 'dark' : 'light';
+		document.querySelector('HTML').setAttribute('class', theme);
+	};
+
+	onMount(() => {
+		theme = document.querySelector('HTML').classList[0] || 'light';
+	});
 </script>
+
+<Button sm on:click={handleDirection}>{dir}</Button>
+<Button sm on:click={handleTheme}>{theme}</Button>
 
 <h1>Welcome to SvelteKit</h1>
 <Button>base</Button>
+<Button inverted>base</Button>
