c65517cd57b65bab97917cabd35762bcbfca7a0a
mohammad esmaiel emadi <emadi68moh@gmail.com>
2021-10-30 06:06:02 +0330

    add button early doc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import { Button } from '../lib';
+	import { Button, Div } from '$lib';
 
 	let dir: 'rtl' | 'ltr' = 'ltr';
 	let theme = '';
@@ -21,11 +21,81 @@
 	onMount(() => {
 		theme = document.querySelector('HTML').classList[0] || 'light';
 	});
+
+	/* <h1>inverted</h1> */
+	/* <Button inverted>inverted</Button> */
 </script>
 
-<Button sm on:click={handleDirection}>{dir}</Button>
-<Button sm on:click={handleTheme}>{theme}</Button>
+<Div background>
+	<Button sm on:click={handleDirection}>{dir}</Button>
+	<Button sm on:click={handleTheme}>{theme}</Button>
 
-<h1>Welcome to SvelteKit</h1>
-<Button>base</Button>
-<Button inverted>base</Button>
+	<h1>Welcome to SvelteKit</h1>
+	<Div background>
+		<p>basic</p>
+		<Button base>base</Button>
+		<Button primary>primary</Button>
+		<Button success>success</Button>
+		<Button warning>warning</Button>
+		<Button danger>danger</Button>
+		<Button info>info</Button>
+		<Button link>link</Button>
+	</Div>
+	<Div background>
+		<p>light</p>
+		<Button light base>base</Button>
+		<Button light primary>primary</Button>
+		<Button light success>success</Button>
+		<Button light warning>warning</Button>
+		<Button light danger>danger</Button>
+		<Button light info>info</Button>
+		<Button light link>link</Button>
+	</Div>
+
+	<Div background>
+		<p>outlinded</p>
+		<Button outlined base>base</Button>
+		<Button outlined primary>primary</Button>
+		<Button outlined success>success</Button>
+		<Button outlined warning>warning</Button>
+		<Button outlined danger>danger</Button>
+		<Button outlined info>info</Button>
+		<Button outlined link>link</Button>
+	</Div>
+
+	<Div background>
+		<p>ghost</p>
+		<Button ghost base>base</Button>
+		<Button ghost primary>primary</Button>
+		<Button ghost success>success</Button>
+		<Button ghost warning>warning</Button>
+		<Button ghost danger>danger</Button>
+		<Button ghost info>info</Button>
+		<Button ghost link>link</Button>
+	</Div>
+
+	<Div background>
+		<p>dark</p>
+		<Button dark base>base</Button>
+		<Button dark primary>primary</Button>
+		<Button dark success>success</Button>
+		<Button dark warning>warning</Button>
+		<Button dark danger>danger</Button>
+		<Button dark info>info</Button>
+		<Button dark link>link</Button>
+	</Div>
+
+	<Div background>
+		<p>sizes</p>
+		<Button xs>base</Button>
+		<Button sm>primary</Button>
+		<Button md>success</Button>
+		<Button lg>warning</Button>
+		<Button xl>danger</Button>
+	</Div>
+
+	<Div background>
+		<p>other</p>
+		<Button block>block</Button>
+	</Div>
+</Div>
